c5f6ec3c82403e8387bdb8168a238a1fdb215f77
Tails <gianma.bennici@gmail.com>
2022-02-11 16:04:10 +0100

    added command folder

---
+++ bot.js
@@ -0,0 +1,39 @@
+
+const dotenv = require('dotenv');
+const fs= require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+
+
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+bot.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	bot.commands.set(command.data.name, command);
+}
+
+bot.on('ready', () => {
+    console.log(`${bot.user.tag} has logged in.`);
+});
+
+bot.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = bot.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+bot.login(token);
