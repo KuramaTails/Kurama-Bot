0b00af09b16ed628937b0d8bd332783a20139273
Tails <gianma.bennici@gmail.com>
2022-02-11 13:31:50 +0100

    Created interactions

--- source/bot.js
+++ source/bot.js
@@ -10,7 +10,27 @@ const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
 dotenv.config();
 
 
-bot.once('ready', () => {
-    console.log('Ready!');
+bot.on('ready', () => {
+    console.log(`${bot.user.tag} has logged in.`);
 });
+
+bot.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nCreated at: ${interaction.user.createdAt}`);
+	}
+});
+
+
+bot.on('message', (message) => {
+    console.log(message.content);
+});
+
 bot.login(token);
