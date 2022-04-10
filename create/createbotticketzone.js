dd614b56c72bc4f415e45ebe1203820aeafc78dc
Tails <gianma.bennici@gmail.com>
2022-04-10 16:12:28 +0200

    added missing ticket zone creation strings

--- create/createbotticketzone.js
+++ create/createbotticketzone.js
@@ -5,12 +5,12 @@ module.exports = {
         .setColor('#0099ff')
         .setTitle("Bot Kurama : Ticket Zone")
         .setURL("https://discord.js.org/#/docs/main/stable/class/MessageEmbed")
-        .addField("Click here for creating a ticket zone:","this will be used by members for any issue")
+        .addField(lang.get(interaction.guild.lang).settings["createTicketZoneField1"],lang.get(interaction.guild.lang).settings["createTicketZoneField2"])
         const buttons = new MessageActionRow()
         buttons.addComponents(
             new MessageButton()
             .setCustomId(`bot-ticketZone`)
-            .setLabel("Create Ticket Zone")
+            .setLabel(lang.get(interaction.guild.lang).settings["createTicketZoneBtn"])
             .setStyle(`SECONDARY`),
         )
         channel.send({embeds:[Embed],components:[buttons]})
