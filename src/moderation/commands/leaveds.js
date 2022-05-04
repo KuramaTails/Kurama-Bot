4079a07d79d61af2e2dcf1f2b4e33c508635ad43
Tails <gianma.bennici@gmail.com>
2022-05-04 16:14:50 +0200

    last update before release

--- src/moderation/commands/leaveds.js
+++ src/moderation/commands/leaveds.js
@@ -11,17 +11,26 @@ module.exports = {
             await createdChannel.forEach(async category => {
                 var par = interaction.guild.channels.cache.find(channel =>channel.name == category)
                 if (par) {
-                    await par.children.forEach(async channel => await channel.delete())
-                    await par.delete()
+                    await par.children.forEach(async channel => {
+                        try {
+                            await channel.delete()
+                        } catch (error) {
+                            console.log(error)
+                        }
+                    })
+                    try {
+                        await par.delete()
+                    } catch (error) {
+                        console.log(error)
+                    }
                 }
             });
             var memberRole = await listRoles.find(role => role.name.includes("Member"))
-            if(memberRole) memberRole.delete() 
+            if(memberRole) await memberRole.delete() 
             var mutedRole = await listRoles.find(role => role.name.includes("Muted"))
-            if(mutedRole) mutedRole.delete()
+            if(mutedRole) await mutedRole.delete()
             var newChannels = await interaction.guild.channels.fetch()
             var selectedChannel = await newChannels.find(channel => channel.type== "GUILD_TEXT")
-            console.log(interaction.guild.settings)
             await selectedChannel.send(lang.get(interaction.guild.settings.lang).commands.moderation["optLeaveDs"])
             await interaction.guild.leave();
         } catch (error) {
