43b154a93d56e41c32a7d007cc49130601354155
Tails <gianma.bennici@gmail.com>
2022-05-07 13:13:23 +0200

    added multiple add/delete on twitch notif

--- src/settings/twitch/addstreamer.js
+++ src/settings/twitch/addstreamer.js
@@ -6,36 +6,56 @@ module.exports = {
 	async execute(modal,lang,twitch) {
         var twitchPlugin = modal.guild.settings.plugins.twitchPlugin
         if (!twitchPlugin.channelId) return modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["selectChannel"], ephemeral: true })
-        var streamerUsername = modal.getTextInputValue('textinput-customid')
-        var query = await twitch.searchChannels({ query: streamerUsername })
-        var selectUser = query.data.find(channel => channel.broadcaster_login == streamerUsername)
-        if (!selectUser) return modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["notFound"], ephemeral: true })
-        if (twitchPlugin.streamerList) {
-            if (twitchPlugin.streamerList.find(streamer=> streamer.broadcaster_login == selectUser.broadcaster_login)) return modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["alreadyAdded"], ephemeral: true })
-        }
-        selectUser.alreadySend=selectUser.is_live 
-        if (!twitchPlugin.streamerList) twitchPlugin.streamerList=[]
-        twitchPlugin.streamerList.push(selectUser)
-        var selectUserDb= {
-            broadcaster_login:selectUser.broadcaster_login,
-            display_name:selectUser.display_name
-        }
-        await dbconnect()
-        await guildSchema.findOneAndUpdate({
-            _id: modal.member.guild.id,
-            }, {
-                $push: {
-                    "plugins.twitchPlugin.streamerList": selectUserDb,
+        var str = modal.getTextInputValue('textinput-customid')
+        var str1 = str.replace(/ /g,'')
+        var str2 = str1.toLowerCase()
+        var streamerUsername = str2.split(",")
+        var pushed = ''
+        var replied = false
+        for (const streamer of streamerUsername) {
+            var query = await twitch.searchChannels({ query: streamer })
+            var selectUser = await query.data.find(channel => channel.broadcaster_login == `${streamer}`)
+            if (!selectUser) return modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["notFound"], ephemeral: true })
+            if (twitchPlugin.streamerList) {
+                if (twitchPlugin.streamerList.find(streamer=> streamer.broadcaster_login == selectUser.broadcaster_login)) {
+                    replied = true
+                    continue
                 }
-            }, {
-                upsert:true,
             }
-        )
-        await dbdisconnect()
-        var updateEmbed = modal.message.embeds[0]
-        updateEmbed.fields[0].value+=`\n- ${selectUser.display_name}`
+            selectUser.alreadySend=selectUser.is_live 
+            if (!twitchPlugin.streamerList) twitchPlugin.streamerList=[]
+            twitchPlugin.streamerList.push(selectUser)
+            pushed+= `${selectUser.display_name},`
+            var selectUserDb= {
+                broadcaster_login:selectUser.broadcaster_login,
+                display_name:selectUser.display_name
+            }
+            await dbconnect()
+            await guildSchema.findOneAndUpdate({
+                _id: modal.member.guild.id,
+                }, {
+                    $push: {
+                        "plugins.twitchPlugin.streamerList": selectUserDb,
+                    }
+                }, {
+                    upsert:true,
+                }
+            )
+            await dbdisconnect()
+            var updateEmbed = modal.message.embeds[0]
+            updateEmbed.fields[0].value+=`\n- ${selectUser.display_name}`
+        }
         await modal.message.edit({embeds:[updateEmbed]})
-        await modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["added"]+` ${selectUser.display_name}.`, ephemeral: true })
-        console.log(`Added streamer notification in ${modal.guild.name}`) 
+        switch (replied) {
+            case true:
+                var reply1=`One or more were already added,this are the added one: ${pushed}.`
+                var reply2=lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["alreadyAdded"]
+                streamerUsername.length>1? modal.editReply({ content:reply1, ephemeral: true }) : modal.editReply({ content:reply2, ephemeral: true })
+            break;
+            case false:
+                modal.editReply({ content:lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["added"]+` ${pushed}.`, ephemeral: true })
+            break;
+        }
+        console.log(`Added streamer notification in ${modal.guild.name}`)
 	}
 };
--- src/settings/twitch/deletestreamer.js
+++ src/settings/twitch/deletestreamer.js
@@ -7,31 +7,51 @@ module.exports = {
         var twitchPlugin = modal.guild.settings.plugins.twitchPlugin
         if (!twitchPlugin.channelId) return
         if (!twitchPlugin.streamerList) return
-        var streamerUsername = modal.getTextInputValue('textinput-customid')
-        var query = await twitch.searchChannels({ query: streamerUsername })
-        var selectUser = query.data.find(channel => channel.broadcaster_login == streamerUsername)
-        if (!selectUser) return modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["notFound"], ephemeral: true })
-        if (twitchPlugin.streamerList) {
-            if (!twitchPlugin.streamerList.find(streamer=> streamer.broadcaster_login == selectUser.broadcaster_login)) return modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["notListed"], ephemeral: true })
-            twitchPlugin.streamerList.pop(selectUser)
-            await dbconnect()
-            await guildSchema.findOneAndUpdate({
-                _id: modal.member.guild.id,
-                }, {
-                    $pull: {
-                        "plugins.twitchPlugin.streamerList": {broadcaster_login:selectUser.broadcaster_login},
-                    }
-                }, {
-                    upsert:true,
+        var str = modal.getTextInputValue('textinput-customid')
+        var str1 = str.replace(/ /g,'')
+        var str2 = str1.toLowerCase()
+        var streamerUsername = str2.split(",")
+        var removed = ''
+        var replied = false
+        for (const streamer of streamerUsername) {
+            var query = await twitch.searchChannels({ query: streamer })
+            var selectUser = query.data.find(channel => channel.broadcaster_login == `${streamer}`)
+            if (!selectUser) return modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["notFound"], ephemeral: true })
+            if (twitchPlugin.streamerList) {
+                if (!twitchPlugin.streamerList.find(streamer=> streamer.broadcaster_login == selectUser.broadcaster_login)) {
+                    replied = true
+                    continue
                 }
-            )
-            await dbdisconnect()
-            var updateEmbed = modal.message.embeds[0]
-            var str = updateEmbed.fields[0].value
-            updateEmbed.fields[0].value = str.replace(`\n- ${selectUser.display_name}`, "");
-            await modal.message.edit({embeds:[updateEmbed]})
-            await modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["foundDelete"]+` ${selectUser.display_name}.`, ephemeral: true })
-            console.log(`Deleted streamer notification in ${modal.guild.name}`) 
+                twitchPlugin.streamerList.pop(selectUser)
+                removed+= `${selectUser.display_name},`
+                await dbconnect()
+                await guildSchema.findOneAndUpdate({
+                    _id: modal.member.guild.id,
+                    }, {
+                        $pull: {
+                            "plugins.twitchPlugin.streamerList": {broadcaster_login:selectUser.broadcaster_login},
+                        }
+                    }, {
+                        upsert:true,
+                    }
+                )
+                await dbdisconnect()
+                var updateEmbed = modal.message.embeds[0]
+                var str = updateEmbed.fields[0].value
+                updateEmbed.fields[0].value = str.replace(`\n- ${selectUser.display_name}`, "");
+            }
         }
+        if(removed.length>1) await modal.message.edit({embeds:[updateEmbed]})
+        switch (replied) {
+            case true:
+                var reply1=`One or more were not in notification list,this are the deleted one: ${removed}.`
+                var reply2=lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["notListed"]
+                streamerUsername.length>1? modal.editReply({ content:reply1, ephemeral: true }) : modal.editReply({ content:reply2, ephemeral: true })
+            break;
+            case false:
+                modal.editReply({ content: lang.get(modal.guild.settings.lang).settings.plugins.twitchPlugin.replies["foundDelete"]+` ${removed}.`, ephemeral: true })
+            break;
+        }
+        console.log(`Deleted streamer notification in ${modal.guild.name}`) 
 	}
 };
