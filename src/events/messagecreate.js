52c4f1b8a8839732e1af1afcb2a023a968cfb9a7
Tails <gianma.bennici@gmail.com>
2022-05-13 16:51:06 +0200

    added tts,fixed pause/resume when player is playing

--- src/events/messagecreate.js
+++ src/events/messagecreate.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
 const bot = require("../../bot");
 const spamcheck = require("../spam/spamcheck");
+const googleTTS = require('google-tts-api');
+const {createAudioResource, getVoiceConnection} = require("@discordjs/voice");
+const mm = require('music-metadata');
 module.exports = {
 	name: 'messageCreate',
     async execute(message) {
@@ -7,6 +11,29 @@ module.exports = {
             await spamcheck.execute(message,bot.spamList,bot.lang)
             if(message.content.startsWith(bot.prefix)){
                 console.log(message.guild.settings)
+                var connection = getVoiceConnection(message.guild.id);
+                if (!connection || connection.joinConfig.channelId != message.member.voice.channelId) return
+                await googleTTS.getAudioBase64(message.content, { lang: message.guild.settings.lang, slow: false })
+                .then((base64) => {
+                  const buffer = Buffer.from(base64, 'base64');
+                  fs.writeFileSync('./src/sounds/tts.mp3', buffer, { encoding: 'base64' });
+                })
+                .catch(console.error);
+                
+                var resource= createAudioResource(fs.createReadStream('src/sounds/tts.mp3'));
+                var metadata = await mm.parseFile('./src/sounds/tts.mp3');
+                resource.volume=message.guild.settings.plugins.playerPlugin.volume/100;
+                if(bot.player.queues.collection.get(message.guild.id)) {
+                    var queue = bot.player.queues.collection.get(message.guild.id)
+                    queue.pause()
+                }
+                connection.subscribe(bot.audioPlayer);
+                bot.audioPlayer.play(resource);
+                setTimeout(async () => {
+                    var music = await bot.player.voices.join(message.member.voice.channel)
+                    connection.subscribe(music.audioPlayer)
+                    if(bot.player.queues.collection.get(message.guild.id)) queue.resume()
+                }, (metadata.format.duration+0.5)*1000);
             }
         }
 	}
--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,31 +1,42 @@
 const {createAudioResource, getVoiceConnection} = require("@discordjs/voice");
-const { createReadStream } = require('fs')
+const { createReadStream } = require('fs');
 const bot = require("../../bot");
+const mm = require('music-metadata');
+
 module.exports = {
     name: 'voiceStateUpdate',
 	async execute(oldState, newState) {
         if (oldState.id==bot.client.application.id) return
-        //if(bot.player.queues.collection.get(oldState.guild.id)) return
         if (oldState.channel != newState.channel) {
             var connection = getVoiceConnection(oldState.guild.id);
             if (!connection || connection.joinConfig.channelId != oldState.channelId && connection.joinConfig.channelId != newState.channelId) return
             var resource 
+            var channel
+            var metadata
             switch (connection.joinConfig.channelId==newState.channelId) {
                 case true:
                     resource=createAudioResource(createReadStream('src/sounds/connected.wav'))
+                    metadata = await mm.parseFile('src/sounds/connected.wav');
+                    channel = newState.channel
                 break;
                 case false:
                     resource=createAudioResource(createReadStream('src/sounds/disconnected.wav'))
+                    metadata = await mm.parseFile('src/sounds/disconnected.wav');
+                    channel = oldState.channel
                 break;
             }
             
             if(bot.player.queues.collection.get(oldState.guild.id)) {
                 var queue = bot.player.queues.collection.get(oldState.guild.id)
                 queue.pause()
-                connection.subscribe(bot.audioPlayer);
-                bot.audioPlayer.play(resource);
-                queue.resume()
             }
+            connection.subscribe(bot.audioPlayer);
+            bot.audioPlayer.play(resource);
+            setTimeout(async () => {
+                var music = await bot.player.voices.join(channel)
+                connection.subscribe(music.audioPlayer)
+                if(bot.player.queues.collection.get(oldState.guild.id)) queue.resume()
+            }, (metadata.format.duration+0.5)*1000);
         }
 	}
 };
